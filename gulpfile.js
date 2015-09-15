ec7030de661c57f341918a425a8c01c06b3124c1
root <root@localhost.localdomain>
2015-09-15 19:06:33 -0300

    Movie list finished. Starting with individual movie page using page.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var gulpIf = require('gulp-if');
 var reload = browserSync.reload;
 var src = {
-    scss: 'app/scss/*.scss',
-    css:  'app/css',
-    html: 'app/*.html',
-    js: 'app/*.js'
+    scss: 'src/scss/*.scss',
+    css:  'src/css',
+    html: 'src/*.html',
+    js: 'src/js/*.js'
 };
 /////////////////////////////////////////////
 // Basic structure of a Gulp task
@@ -33,19 +37,31 @@ gulp.task('hello', function() {
 });
 
 gulp.task('sass', function(){
-  return gulp.src('app/scss/**/*.scss')
+  return gulp.src('src/scss/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('src/css'))
     .pipe(reload({stream: true}));
 });
 
 gulp.task('reload', ['sass'], function() {
   browserSync({
       server: {
-        baseDir: 'app'
+        baseDir: 'src'
       }
   });
   gulp.watch(src.scss, ['sass']);
   gulp.watch(src.html).on('change', reload);
   gulp.watch(src.js).on('change', reload);
+});
+
+gulp.task('useref', function(){
+  var assets = useref.assets();
+
+  return gulp.src('src/*.html')
+    .pipe(assets)
+    .pipe(gulpIf('*.css', minifyCss()))
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(assets.restore())
+    .pipe(useref())
+    .pipe(gulp.dest('dist'));
 });
